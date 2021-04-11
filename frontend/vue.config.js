361d5c95039137501d25156218ef6b6b2abde576
Tim Weber <mail@timweber.name>
2021-04-11 13:46:25 +0200

    Fix Pages deployment expecting to live at root

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,4 +1,10 @@
 module.exports = {
+  // The GitHub Pages deployment doesn't use the domain root, but the
+  // project name, as its root dir. However, the dev server should use the root.
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/webusb/'
+    : '/',
+
   transpileDependencies: [
     'vuetify'
   ]
