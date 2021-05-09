0fddd29b20f493a6264a3594777c8c5d776f9711
Tim Weber <mail@timweber.name>
2021-05-09 18:19:55 +0200

    Add SCPI parsing for IDN and RST

--- firmware/serial_scpi.c
+++ firmware/serial_scpi.c
@@ -1,15 +1,47 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
-#include "hardware/gpio.h"
 #include "pico/binary_info.h"
 #include "scpi/scpi.h"
 
-const uint LED_PIN = 25;
 
+
+// "IDN?" fields. Their meanings are "suggestions" in the standard.
+#define SCPI_IDN1 "cronologic"         /* Manufacturer */
+#define SCPI_IDN2 "WebUSB Experiment"  /* Model */
+#define SCPI_IDN3 NULL                 /* no suggestion */
+#define SCPI_IDN4 "0.1"                /* Firmware level */
+
+// This will hold the SCPI "instance".
 scpi_t scpi_context;
+
+// Input buffer for reading SCPI commands.
+#define SCPI_INPUT_BUFFER_SIZE 256
+static char scpi_input_buffer[SCPI_INPUT_BUFFER_SIZE];
+
+// Error queue required by the library. Size taken from the docs.
+#define SCPI_ERROR_QUEUE_SIZE 17
+scpi_error_t scpi_error_queue[SCPI_ERROR_QUEUE_SIZE];
+
+// The function called by the SCPI library when it wants to send data.
+size_t write_scpi(scpi_t *context, const char *data, size_t len) {
+	return fwrite(data, 1, len, stdout);
+}
+
+// The SCPI commands we support and the callbacks they use.
 scpi_command_t scpi_commands[] = {
 	{ .pattern = "*IDN?", .callback = SCPI_CoreIdnQ, },
+	{ .pattern = "*RST",  .callback = SCPI_CoreRst, },
+	SCPI_CMD_LIST_END
 };
+
+// Additional callbacks to be used by the library.
+scpi_interface_t scpi_interface = {
+	.write = write_scpi,
+	.error = NULL,
+	.reset = NULL,
+};
+
+
 
 int main() {
 	bi_decl(bi_program_description("This is a test binary, including the SCPI library."));
@@ -17,13 +49,19 @@ int main() {
 
 	stdio_init_all();
 
-	gpio_init(LED_PIN);
-	gpio_set_dir(LED_PIN, GPIO_OUT);
+	// Initialize the SCPI library.
+	SCPI_Init(
+		&scpi_context, scpi_commands, &scpi_interface, scpi_units_def,
+		SCPI_IDN1, SCPI_IDN2, SCPI_IDN3, SCPI_IDN4,
+		scpi_input_buffer, SCPI_INPUT_BUFFER_SIZE,
+		scpi_error_queue, SCPI_ERROR_QUEUE_SIZE
+	);
+
 	while (1) {
-		gpio_put(LED_PIN, 0);
-		sleep_ms(250);
-		gpio_put(LED_PIN, 1);
-		puts("Hello World!\n");
-		sleep_ms(750);
+		// Feed single characters into the SCPI library.
+		// This is somewhat discouraged, since (according to the docs) it will
+		// apparently cause the whole buffer to be re-parsed after each character.
+		char c = fgetc(stdin);  // TODO: Error handling.
+		SCPI_Input(&scpi_context, &c, 1);
 	}
 }
